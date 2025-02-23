d5206ff70affca62282ef4d061b1cb5c46b60a2d
CodeDesignPlus <codedesignplus@outlook.com>
2025-02-23 13:01:49 -0500

    fix: update package references to version 0.6.0-beta.18 for improved compatibility and performance

--- src/entrypoints/CodeDesignPlus.Net.Microservice.Rest/Program.cs
+++ src/entrypoints/CodeDesignPlus.Net.Microservice.Rest/Program.cs
@@ -1,17 +1,10 @@
 using CodeDesignPlus.Net.Microservice.Commons.EntryPoints.Rest.Middlewares;
 using CodeDesignPlus.Net.Microservice.Commons.EntryPoints.Rest.Swagger;
 using CodeDesignPlus.Net.Microservice.Commons.FluentValidation;
+using CodeDesignPlus.Net.Microservice.Commons.HealthChecks;
 using CodeDesignPlus.Net.Microservice.Commons.MediatR;
-using CodeDesignPlus.Net.Mongo.Abstractions.Options;
-using CodeDesignPlus.Net.RabbitMQ.Abstractions;
-using CodeDesignPlus.Net.RabbitMQ.Abstractions.Options;
-using CodeDesignPlus.Net.Redis.Abstractions;
 using CodeDesignPlus.Net.Redis.Cache.Extensions;
-using CodeDesignPlus.Net.Redis.Options;
 using CodeDesignPlus.Net.Vault.Extensions;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using MongoDB.Driver;
 using NodaTime.Serialization.SystemTextJson;
 
 
@@ -41,39 +34,12 @@ builder.Services.AddMediatR<CodeDesignPlus.Net.Microservice.Application.Startup>
 builder.Services.AddSecurity(builder.Configuration);
 builder.Services.AddCoreSwagger<Program>(builder.Configuration);
 builder.Services.AddCache(builder.Configuration);
-builder.Services.AddHealthChecks()
-    .AddRedis(x =>
-    {
-        var factory = x.GetRequiredService<IRedisFactory>();
-
-        return factory.Create(FactoryConst.RedisCore).Connection;
-    }, name: "Redis", tags: ["ready"])
-    .AddRabbitMQ(x =>
-    {
-        var raabbitConnection = x.GetRequiredService<IRabbitConnection>();
-
-        return raabbitConnection.Connection;
-    }, name: "RabbitMQ", tags: ["ready"])
-    .AddMongoDb(x =>
-    {
-        var mongoClient = x.GetRequiredService<IMongoClient>();
-
-        return mongoClient;
-    }, name: "MongoDB", tags: ["ready"])
-    .AddCheck("self", () => HealthCheckResult.Healthy(), tags: ["live"]);
-
+HealthChecksExtensions.AddHealthChecks(builder.Services);
 
 var app = builder.Build();
 
-app.MapHealthChecks("/health/ready", new HealthCheckOptions
-{
-    Predicate = healthCheck => healthCheck.Tags.Contains("ready")
-});
+app.UseHealthChecks();
 
-app.MapHealthChecks("/health/live", new HealthCheckOptions
-{
-    Predicate = healthCheck => healthCheck.Tags.Contains("live"), // Solo checks con el tag "live"
-});
 app.UseCoreSwagger();
 
 app.UseMiddleware<ExceptionMiddleware>();
